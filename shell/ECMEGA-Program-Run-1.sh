817e46a79e32cec78881a4dcea262d83c6758b30
david-story <story.a.david@gmail.com>
2019-01-15 08:20:47 -0800

    file changes

---
+++ shell/ECMEGA-Program-Run-1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+sudo mkdir "software"
+sudo mkdir "output"
+sudo mkdir "log"
+pyout=`date +"PythonOutput-"%Y-%m-%d.%H:%M:%S".txt"`
+pyexit=`date +"ExitOutput-"%Y-%m-%d.%H:%M:%S".txt"`
+sudo python ServerTesters.py > $pyout
+sudo echo $? > $pyexit
+sudo mv $pyout log/
+sudo mv $pyexit output/
+tarname=`date +"SERVER_1_RUN_FILES_"%Y_%m_%d_%H_%M_%S".tgz"`
+tar -zcvf $tarname .
+sudo aws s3 cp $tarname s3://ecmega-project-bucket/server-outputs/
